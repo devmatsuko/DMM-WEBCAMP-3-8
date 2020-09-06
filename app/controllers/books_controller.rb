51cd1c6126d4259e284c76e818fc70ee8e29783f
EC2 Default User <ec2-user@ip-172-31-76-156.ec2.internal>
2020-09-06 02:34:55 +0000

    [Add] indexページの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,19 @@
 class BooksController < ApplicationController
+
   def index
+    # 全Bookデータを取得し、インスタンスに格納
+    @books = Book.all
+    # 新規投稿用にインスタンスを作成
+    @book = Book.new
+  end
+
+  def create
+    # データを新規登録するためのインスタンス作成
+    book = Book.new(book_params)
+    # データをデータベースに保存するためのsaveメソッド実行
+    book.save
+    # トップ画面へリダイレクト
+    redirect_to books_path
   end
 
   def show
@@ -7,4 +21,10 @@ class BooksController < ApplicationController
 
   def edit
   end
+
+  private
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+
 end
